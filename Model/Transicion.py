b7379e12f80ce037e86f402894532622b525ed1c
Shiome Diaz <49491301+ShiomeDiaz@users.noreply.github.com>
2021-10-21 11:07:51 -0500

    Estructuracion
    se estructura de manera MVC
    se Programa el nodoEstado y Transicion

---
+++ Model/Transicion.py
@@ -0,0 +1,23 @@
+class Transicion:
+    def __init__(self, origen, destino, operacion):
+        self.origen = origen
+        self.destino = destino
+        self.operacion = operacion
+
+    def getOrigen(self):
+        return self.origen
+
+    def setOrigen(self, origen):
+        self.origen = origen
+
+    def getDestino(self):
+        return self.destino
+
+    def setDestino(self, destino):
+        self.destino = destino
+
+    def getOperacion(self):
+        return self.operacion
+
+    def setOperacion(self, operacion):
+        self.operacion = operacion
---
+++ Model/nodoEstado.py
@@ -0,0 +1,23 @@
+class nodoEstado:
+    def __init__(self, estado):
+        self.estado = estado
+        self.listaTransicion = []
+        self.estadoAceptacion = bool
+
+    def getEstado(self):
+        return self.estado
+
+    def setEstado(self, estado):
+        self.estado = estado
+
+    def getListaTransicion(self):
+        return self.listaTransicion
+
+    def setListaTransicion(self, listaTransicion):
+        self.listaTransicion = listaTransicion
+
+    def getEstadoAceptacion(self):
+        return self.estadoAceptacion
+
+    def setEstadoAceptacion(self, estadoAceptacion):
+        self.estadoAceptacion = estadoAceptacion
